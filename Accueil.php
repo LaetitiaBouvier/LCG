739d8748bd50e4c663aeacbc040bb5cdd16b2114
dimitribaron <dimitribaron@MacBook-Pro.local>
2015-12-07 13:37:46 +0100

    Ajout de PHP dans Connexion/Déconnexion

--- Accueil.php
+++ Accueil.php
@@ -1,3 +1,30 @@
+<?php
+
+if (isset($_POST['pseudo']) OR isset($_POST['mdp']))
+{
+  $mdp_hache = sha1($_POST['mdp']);
+
+  $bdd = new PDO('mysql:host=localhost;dbname=connexion_gauloise', 'root', 'root');
+  $req = $bdd->prepare('SELECT id_utilisateur, pseudo_utilisateur FROM utilisateur_table WHERE pseudo_utilisateur = ? AND mdp_utilisateur = ?');
+  $req->execute(array($_POST['pseudo'], $mdp_hache));
+
+  $data = $req->fetch();
+
+  if (!$data)
+  {
+  	echo 'Mauvais identifiant ou mot de passe!';
+  }
+  else
+  {
+  	session_start();
+  	$_SESSION['id_utilisateur'] = $data['id_utilisateur'];
+  	$_SESSION['pseudo_utilisateur'] = $data['pseudo_utilisateur'];
+  	echo 'Bonjour ' . $data['pseudo_utilisateur'] . "! Bienvenue sur La Connexion Gauloise!";
+  }
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -8,14 +35,20 @@
 
 	<body>
 		<header>
-			<div id="bloc_titre_principal">
-				<img src="images/[A1G2E]Logo La Connexion Gauloise2.png" alt="titre principal" />
-				<ul>
-					<li id="se_connecter"><a href="Log-In.php">Se Connecter</a></li>
-					<li id="s_inscrire"><a href="Create-Profil.php">S'inscrire</a></li>
-					<li id="creer_un_evenement"><a href="Create-Event.php">Créer un événement</a></li>
-				</ul>
-			</div>
+
+<?php
+
+if (!isset($_SESSION['id_utilisateur']))
+{
+  include("signinup.html");
+}
+else
+{
+  echo "<a href='signout.php'>Deconnexion</a>";
+}
+
+?>
+
 			<div id="barre_recherche">
 				<form method="post" action="page_fictive.php" id="bloc_barre_de_recherche">
 					 <input type="search" name="barre_de_recherche" id="barre_de_recherche" size="50" placeholder=" Rechercher un membre ou un événement" />
