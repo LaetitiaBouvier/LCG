6cc6caf117fffcc285d329cf2d52bb6c753957af
LaetitiaBouvier <laetitia.bouvier@isep.fr>
2015-12-07 17:54:26 +0100

    Rename

--- Create-Event.php
+++ Create-Event.php
@@ -1,6 +1,6 @@
 <!-- insertion de la fonction permettant d'ajouter des utilisateurs à la base de données !-->
 <?php
-	require 'FonctionsEventTRY.php';
+	require 'FonctionsEvent.php';
 	insert_events();
 ?>
 
--- FonctionsEvent.php
+++ FonctionsEvent.php
@@ -13,7 +13,7 @@ function insert_events()
   { $connect = mysqli_connect("localhost", "root", "", "Connexion_Gauloise"); // mdp = "root", "pass" ou encore "" (A MODIFIER SELON VOTRE ORDI)
 
 
-  if (isset($_POST['valider']) && $_POST['valider'] == "EVENEMENT")
+  if (isset($_POST['valider']) && $_POST['valider'] == "VALIDER MON EVENEMENT")
     { $cond=true; // Vérifie que le formulaire soit bien validé (absence du "else" plus tard)
 
 
@@ -21,7 +21,7 @@ function insert_events()
   	if(isset($_POST['nom'])						&& !empty($_POST['nom']))
       {	$nom=$_POST['nom'];}
     else
-      { $cond = false; print "Le nom de l'evenement n'est pas configuré ! ";}
+      { print "Le nom de l'evenement n'est pas configuré ! ";}
 
 
   // DESCRIPTION DE L'EVENEMENT
@@ -35,7 +35,7 @@ function insert_events()
     if(isset($_POST['photo'])						 && !empty($_POST['photo']))
       {	$photo=$_POST['photo'];}
     else
-      { $cond = false; print "La photo de l'evenement n'est pas configurée ! ";}
+      { print "La photo de l'evenement n'est pas configurée ! ";}
 
 
   // AJOUTER UNE VIDEO
@@ -49,7 +49,7 @@ function insert_events()
     if(isset($_POST['categorie'])							 && !empty($_POST['categorie']))
       {	$categorie=$_POST['categorie'];}
     else
-        { $cond = false; print "La catégorie de l'evenement n'est pas configurée ! ";}
+        { print "La catégorie de l'evenement n'est pas configurée ! ";}
 
   // CIBLE DE L'EVENEMENT
     $cible = "";
@@ -64,7 +64,7 @@ function insert_events()
     if(isset($_POST['endroit'])						&& !empty($_POST['endroit']))
       {	$endroit=$_POST['endroit'];}
     else
-      { $cond = false; print "L'endroit de l'evenement n'est pas configuré ! ";}
+      { print "L'endroit de l'evenement n'est pas configuré ! ";}
 
 
   // RUE
@@ -76,12 +76,13 @@ function insert_events()
     if(isset($_POST['CP'])						&& !empty($_POST['CP']))
       { $CP=$_POST['CP'];}
       if(strlen($CP) != 5 AND !is_numeric($CP))
-        { $cond = false; print "Le code postal de l'évenement n'est pas configuré ! N'est pas un entier";}
+        { print "Le code postal de l'évenement n'est pas configuré ! N'est pas un entier";}
 
 
   // VILLE
     if(isset($_POST['ville'])						&& !empty($_POST['ville']))
       {	$ville=$_POST['ville'];}
+    else{$ville = NULL;}
 
   // DEPARTEMENT
   $departement=NULL;
@@ -95,50 +96,51 @@ function insert_events()
     if(isset($_POST['datedeb'])			&& !empty($_POST['datedeb']))
       { $datedeb=$_POST['datedeb'];}
     else
-      { $cond = false; print "La date de début de l'évenement n'est pas configurée ! ";}
+      { print "La date de début de l'évenement n'est pas configurée ! ";}
 
   // HEURE DEBUT EVENT
   if(isset($_POST['heuredeb'])			&& !empty($_POST['heuredeb']))
     { $heuredeb=$_POST['heuredeb'];}
+  else{$heuredeb = NULL;}
 
   // DATE FIN EVENT
   if(isset($_POST['datefin'])			&& !empty($_POST['datefin']))
     { $datefin=$_POST['datefin'];}
   else
-    { $cond = false; print "La date de fin de l'évenement n'est pas configurée ! ";}
+    { print "La date de fin de l'évenement n'est pas configurée ! ";}
 
   // HEURE FIN EVENT
   if(isset($_POST['heurefin'])			&& !empty($_POST['heurefin']))
     { $heurefin=$_POST['heurefin'];}
+  else{ $heurefin = NULL;}
 
   // NOMBRE PARTICIPANTS
   if(isset($_POST['participants'])			&& !empty($_POST['participants']))
     { $participants=$_POST['participants'];}
+  else{ $participants = NULL;}
 
   //EVENEMENT PAYANT
     $payant=NULL;
   // LIEN
   if(isset($_POST['website'])			&& !empty($_POST['website']))
-    { $heurefin=$_POST['website'];}
+    { $website=$_POST['website'];}
+  else{ $website = NULL; }
 
-
+    $organisateur = NULL;
     // ENREGISTREMENT DANS LA BASE DE DONNEES
-  		if($cond == true)
-        {        mysqli_query($connect, "insert into evenement_table (Nom_Evenement, Organisateur, Image_Evenement, Video_Evenement, JourDebut_Evenement,
+
+         mysqli_query($connect, "insert into evenement_table (Nom_Evenement, Organisateur, Image_Evenement, Video_Evenement, JourDebut_Evenement,
                                   JourFin_Evenement, HeureDebut_Evenement, HeureFin_Evenement, Description_Evenement, NbMaxParticipants_Evenement,
                                   Categorie_Evenement, Cibles_Evenement, NomLieu_Evenement, AdresseRue_Evenement, AdressePostal_Evenement,
                                   AdresseVille_Evenement, AdresseDepartement_Evenement, AdresseRegion_Evenement, Payant_Evenement,
                                   LienSiteWeb_Evenement)
                                 values ('$nom', '$organisateur', '$photo','$video', '$datedeb', '$datefin', '$heuredeb','$heurefin','$description','$participants',
-                                '$categorie', '$cible', '$endroit', '$rue', '$CPK', '$ville', '$departement', '$region', '$payant','$website')")
+                                '$categorie', '$cible', '$endroit', '$rue', '$CP', '$ville', '$departement', '$region', '$payant','$website')")
                                 or die('Error: ' . mysqli_error($connect));
-  		  }
-      else
-        { print "Les informations entrées sont incorrectes ! "; }
 
-  	}
-
-  	mysqli_close($connect);}
+  	mysqli_close($connect);
+  }
+  }
 
 
   ?>
