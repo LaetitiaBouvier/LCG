8e96a86e18173d67a68867adb6e49adbea2b4e1b
ManonT <thibon.manon@gmail.com>
2015-12-07 14:13:59 +0100

    Inscription

--- Page_Inscription.php
+++ Page_Inscription.php
@@ -6,19 +6,30 @@
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http;//www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
-
 	<head>
 		<title> INSCRIPTION </title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 		<link rel="stylesheet" href="Style-form.css"/>
 	</head>
-
 	<body>
-		<centralform>
-		<h2> Formulaire de création/modification de profil </h2>
-		<!-- insertion du formulaire!-->
-		<?php include("Form-Profil.php"); ?>
-	 </centralform>
-	</body>
+		<header>
+			<?php include("header.php"); ?>
+		</header>
 
+		<nav>
+      <?php include("menu_deroulant.php"); ?>
+    </nav>
+
+		<centralform>
+			<div>
+				<h2> Formulaire de création/modification de profil </h2>
+				<!-- insertion du formulaire!-->
+				<?php include("Form-Profil.php"); ?>
+			</div>
+		</centralform>
+
+		<footer>
+			<?php include("footer.php"); ?>
+		</footer>
+	</body>
 </html>
