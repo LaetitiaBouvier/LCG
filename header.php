0e6b5746bb99ca85ef8eeda639884e1db3f7c225
ManonT <thibon.manon@gmail.com>
2015-12-07 19:27:17 +0100

    sdlfjh

--- header.php
+++ header.php
@@ -3,20 +3,25 @@
   <link rel="stylesheet" href="header.css" />
 
   <header>
-    <div id="bloc_titre_principal">
-      <img src="Images_code/nomsiteentier.png" alt="titre principal" />
-      <ul>
-        <li id="se_connecter"><a href="Log-In.php">Se Connecter</a></li>
-        <li id="s_inscrire"><a href="Create-Profil.php">S'inscrire</a></li>
-        <li id="creer_un_evenement"><a href="Create-Event.php">Créer un événement</a></li>
-      </ul>
-    </div>
+    <?php
+        if (!isset($_SESSION['id_utilisateur']))
+    {
+      include("signinup.html");
+    }
+    else
+    {
+      echo "<div id='bloc_titre_principal'><img src='images/[A1G2E]Logo La Connexion Gauloise2.png' alt='titre principal' /><a href='signout.php' id='se_deconnecter'>Se déconnecter</a></div>";
+    }
+    ?>
+
     <div id="barre_recherche">
+
       <form method="post" action="page_fictive.php" id="bloc_barre_de_recherche">
+        <li id="creer_un_evenement"><a href="Create-Event.php">Créer un événement</a></li>
          <input type="search" name="barre_de_recherche" id="barre_de_recherche" size="50" placeholder=" Rechercher un membre ou un événement" />
-         <input type="submit" id="rechercher" value="Rechercher"/>
-        <p id="recherche_avancee">Recherche avancée</p>
-      </form>
+    		 <input type="submit" id="rechercher" value="Rechercher"/>
+    		<p id="recherche_avancee">Recherche avancée</p>
+    	</form>
     </div>
   </header>
 </html>
