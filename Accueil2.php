436e37890ff5477a8973a6f56f8486898bac335c
ManonT <thibon.manon@gmail.com>
2015-12-15 11:33:34 +0100

    MAJ accueil test

--- Accueil2.php
+++ Accueil2.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http;//www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
 	<head>
-		<title> MODIFICATION-OK </title>
+		<title>La Connexion Gauloise</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 		<link rel="stylesheet" href="Style-form.css"/>
 	</head>
@@ -16,85 +16,83 @@
 		</nav>
 
 		<div id="corps">
-			<section id="slideshow">
+		<section id="slideshow">
 
-			<div class="container">
-				<div class="c_slider"></div>
-				<div class="slider">
-					<figure>
-						<a href="@"><img src="images/4-albuquerque-international-balloon-festival__large.jpg" alt="" width="1100" height="600" /></a>
-						<figcaption>
-							<p><h3> Rassemblement - 23 Avril 2016 </h3></p>
-							<p> Paris </p>
-						</figcaption>
-					</figure><!--
-					-->
-					<figure>
-						<a href="@"><img src="images/camping-lac-festival-bordeaux.jpg" alt="" width="1100" height="600"/></a>
-						<figcaption>
-							<p><h3> Concert - 24 Avril 2016 </h3></p>
-							<p> Strasbourg </p>
-						</figcaption>
+		<div class="container">
+			<div class="c_slider"></div>
+			<div class="slider">
+				<figure>
+					<a href="@"><img src="images/4-albuquerque-international-balloon-festival__large.jpg" alt="" width="1100" height="600" /></a>
+					<figcaption>
+						<p><h3> Rassemblement - 23 Avril 2016 </h3></p>
+						<p> Paris </p>
+					</figcaption>
 
-					</figure><!--
-					-->
-					<figure>
-						<a href="@" ><img src="images/concert en plein air.jpg" alt="" width="1100" height="600"/></a>
-						<figcaption>
-							<p><h3> Festival - 24 Avril 2016 </h3></p>
-							<p> Dunkerque </p>
-						</figcaption>
-					</figure>
-				</div>
+				</figure><!--
+				--><figure>
+					<a href="@"><img src="images/camping-lac-festival-bordeaux.jpg" alt="" width="1100" height="600"/></a>
+					<figcaption>
+						<p><h3> Concert - 24 Avril 2016 </h3></p>
+						<p> Strasbourg </p>
+					</figcaption>
+
+				</figure><!--
+				-->
+				<figure>
+					<a href="@" ><img src="images/concert en plein air.jpg" alt="" width="1100" height="600"/></a>
+					<figcaption>
+						<p><h3> Festival - 24 Avril 2016 </h3></p>
+						<p> Dunkerque </p>
+					</figcaption>
+				</figure>
 			</div>
+		</div>
 
-			<div id="cat1">
-				<div id="titre1">
-					<ul>
-						<li> <h3> 17 Octobre 2016 </h3> </li>
-						<li> <h5>Course au jardin du Luxembourg </h5></li>
-					</ul>
-				</div>
-				<img src="images/Marathon.jpg" class="photo1"/>
-				<div id="ssmenu1">
-					<ul>
-						<li> La 21 ème édition des 21km du Luxembourg </li>
-						<li> N'hésitez pas à vous inscrire ! </li>
-					</ul>
-				</div>
+		<div id="cat1">
+			<div id="titre1">
+				<ul>
+					<li> <h3> 17 Octobre 2016 </h3> </li>
+					<li> <h5>Course au jardin du Luxembourg </h5></li>
+				</ul>
 			</div>
+			<img src="images/Marathon.jpg" class="photo1"/>
+			<div id="ssmenu1">
+				<ul>
+					<li> La 21 ème édition des 21km du Luxembourg </li>
+					<li> N'hésitez pas à vous inscrire ! </li>
+				</ul>
+			</div>
+		</div>
 
-			<div id="cat2">
-				<div id="titre2">
-					<ul>
-						<li> <h3> 23 Octobre 2016 </h3> </li>
-						<li> <h5> Soirée Raclette </h5></li>
-					</ul>
-				</div>
-				<img src="Images_code/raclette.jpg" class="photo2"/>
-				<div id="ssmenu2">
-					<ul>
-						<li> Soirée à la salle des fêtes du XVIIIème, Paris</li>
-						<li> Venez nombreux !</li>
-					</ul>
-				</div>
+		<div id="cat2">
+			<div id="titre2">
+				<ul>
+					<li> <h3> 23 Octobre 2016 </h3> </li>
+					<li> <h5> Soirée Raclette </h5></li>
+				</ul>
 			</div>
+			<img src="Images_code/raclette.jpg" class="photo2"/>
+			<div id="ssmenu2">
+				<ul>
+					<li> Soirée à la salle des fêtes du XVIIIème, Paris</li>
+					<li> Venez nombreux !</li>
+			</div>
+		</div>
 
-			<div id="cat3">
-				<div id="titre3">
-					<ul>
-						<li> <h3> 2 Novembre 2016 </h3> </li>
-						<li> <h5> Concert Time Impala </h5></li>
-					</ul>
-				</div>
-				<img src="Images_code/tame-impala-couv.jpg" class="photo3"/>
-				<div id="ssmenu3">
-					<ul>
-						<li> Concert du Groupe de Rock australien </li>
-						<li> Nouvel album : Currents </li>
-					</ul>
-				</div>
+		<div id="cat3">
+			<div id="titre3">
+				<ul>
+					<li> <h3> 2 Novembre 2016 </h3> </li>
+					<li> <h5> Concert Time Impala </h5></li>
+				</ul>
 			</div>
+			<img src="Images_code/tame-impala-couv.jpg" class="photo3"/>
+			<div id="ssmenu3">
+				<ul>
+					<li> Concert du Groupe de Rock australien </li>
+					<li> Nouvel album : Currents </li>
+			</div>
+		</div>
 			</section>
 		</div>
 
