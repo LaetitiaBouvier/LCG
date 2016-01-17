d30e18b2b32ddd6c909e38465c27854f2724a9ef
LaetitiaBouvier <laetitia.bouvier@isep.fr>
2016-01-17 16:39:46 +0100

    pour que ca marche sur windows les coupains

--- delete_msg.php
+++ delete_msg.php
@@ -2,7 +2,7 @@
 	if(isset($_POST["supprmsg"])){
 			$IDT = $_POST["supprmsg"];
 	}
-	$bdd = new PDO('mysql:host=localhost;dbname=connexion_gauloise', 'root', 'root'); /*root pour mac*/
+	$bdd = new PDO('mysql:host=localhost;dbname=connexion_gauloise', 'root', ''); /*root pour mac*/
 	$req=$bdd->prepare('DELETE from topic_table WHERE ID_MSG = ?');
 	$req->execute(array($IDT));
 	header("location:Confirm-Action.php");
