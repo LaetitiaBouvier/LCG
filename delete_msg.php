c624e722f1c30377abd0742cc6b51ff4134a28ff
dimitribaron <dimitribaron@MacBook-Pro.local>
2016-06-07 17:40:47 +0200

    Commit

--- delete_msg.php
+++
@@ -1,9 +0,0 @@
-<?php
-	if(isset($_POST["supprmsg"])){
-			$IDT = $_POST["supprmsg"];
-	}
-	$bdd = new PDO('mysql:host=localhost;dbname=connexion_gauloise', 'root', ''); /*root pour mac*/
-	$req=$bdd->prepare('DELETE from topic_table WHERE ID_MSG = ?');
-	$req->execute(array($IDT));
-	header("location:Confirm-Action.php");
-?>
